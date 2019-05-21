ed0cdf45635dc2f4aa9051a0b87dd819ccc4e25b
SeaWar741 <juanca741@gmail.com>
2019-05-21 09:41:48 -0500

    add study file

---
+++ 6to_semestre/study_java.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+class study_java{
+    int[] perro = new int[5];
+    ArrayList<String> lista = new ArrayList<String>();//add
+    LinkedList<String> lista_enlazada = new LinkedList<String>(); //add
+    Stack<String> STACK = new Stack<String>(); //pop,push
+    
+    public static void main(String[] args) {
+     System.out.print("Hola");
+     for(i=0;i<5;i++){
+
+     }
+     int counter=0;
+     while(counter<=2){
+        counter++;
+     }
+
+    }
+}
+class study_java2 extends study_java{
+    int[][] multi = new int[5][10];
+    public void name() {
+        for(int i =0;i< perro.length;i++){
+
+        }
+        String s = "holas";
+        for (int i = 0; i < s.length(); i++){ //iterar en string
+            char c = s.charAt(i);        
+            //Process char
+            System.out.print(c);
+        }
+        if(lista.isEmpty()){
+            System.out.println("Esta vacia");
+        }
+        
+    }
+}
